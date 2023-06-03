646038fa24540e8aa659cab9257ec0973098068f
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-06-02 21:47:22 -0300

    Mejoria para ejecutar el main sin hacer graficos si no se le pasa argumentos

--- main.m
+++ main.m
@@ -1,9 +1,14 @@
-% Código para octave que intenta resolver el TPU N°2 de Análisis de Sistemas y Señales.
+% Código para Octave que intenta resolver el TPU N°1 de Análisis de Sistemas y Señales.
 % Éste es el script principal que ejecuta el resto de scripts.
+
 close all; clear all; clc;
 
 % Se define una variable global que me permite guardar los archivos solo cuando ejecuto el script para entrega.
-global savePlots = str2num(argv(){1}); 
+if (length(argv()) > 0)
+  global savePlots = str2num(argv(){1}); 
+else
+  global savePlots = 0;
+end
 
 % Cargo las direcciones de todas las dependencias.
 addpath('SCRIPTS:SCRIPTS/utilitarios');
@@ -12,12 +17,8 @@ addpath('./AnSyS2023_PU1_m');
 % Se definen variables globales.
 % packages();
 global_variables();
-disp("lkdsjsalkdja");
-consolelog(cstrcat("exists: ", GENERAL_GRAPH_PATHS));
 % delta_();
 % equations();
-
-consolelog("LASKDJLAKD");
 
 % Se resuelve el ejercicio 1 inciso 1
 ej1_inc1();
