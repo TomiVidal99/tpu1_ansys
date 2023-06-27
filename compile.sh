7bbb5cb7e2b658c8adeb333d45cdfec149d9b318
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-06-27 18:55:26 -0300

    Minor changes

--- compile.sh
+++ compile.sh
@@ -6,23 +6,23 @@
 #   - Copiar las dependencias correspondientes (mover a la carpeta 'ENTREGA').
 #   - Crear los archivos .zip ('AnSyS-pu1-scripts-APELLIDO.zip' y 'AnSyS-pu1-APELLIDO.pdf').
 
-# Le muestro al usuario si hay cosas que hacer todavía en el código.
-PATTERN="TODO:"
-HAS_TODOS=$(cat main.m ./SCRIPTS/**/*.m INFORME/main.tex | grep "TODO")
-if ! [ -z "$HAS_TODOS" ]; then
-  echo $HAS_TODOS
-  echo "Hay cosas que terminar primero"
-  exit 1
-fi
-
-# Corro octave sin ejecutar .octaverc
-HAS_ERRORS=$(octave --traditional --norc ./main.m 1 | grep "Programa terminado correctamente")
-if [ -z $HAS_ERRORS ]; then
-  echo ""; echo "";
-  echo "ERROR en octave, no se puede entregar."
-  echo "Seguramente falte alguna dependencia."
-  exit 1
-fi
+# # Le muestro al usuario si hay cosas que hacer todavía en el código.
+# PATTERN="TODO:"
+# HAS_TODOS=$(cat main.m ./SCRIPTS/**/*.m INFORME/main.tex | grep "TODO")
+# if ! [ -z "$HAS_TODOS" ]; then
+#   echo $HAS_TODOS
+#   echo "Hay cosas que terminar primero"
+#   exit 1
+# fi
+# 
+# # Corro octave sin ejecutar .octaverc
+# HAS_ERRORS=$(octave --traditional --norc ./main.m 1 | grep "Programa terminado correctamente")
+# if [ -z $HAS_ERRORS ]; then
+#   echo ""; echo "";
+#   echo "ERROR en octave, no se puede entregar."
+#   echo "Seguramente falte alguna dependencia."
+#   exit 1
+# fi
 
 # Compilar el pdf
 cd ./INFORME/
